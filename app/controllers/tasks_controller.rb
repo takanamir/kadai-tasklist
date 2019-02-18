d63f5a6d15b10dde4458fa41ef63949f56d519f6
takanamir <rtakanami0706@gmail.com>
2019-02-18 04:07:18 +0000

    tasklist DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
@@ -49,7 +51,10 @@ class TasksController < ApplicationController
 
   private
 
-  # Strong Parameter
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:content)
   end
