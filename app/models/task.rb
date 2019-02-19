67c937704c6b7d5ae5d031244d5412c97dc492b6
takanamir <rtakanami0706@gmail.com>
2019-02-19 01:45:00 +0000

    Fix tasklist not null

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ApplicationRecord
-  validates :status, length: { maximum: 10, too_long: "最大%{count}文字まで使用できます" }
+  validates :status, length: { minimum: 1, too_short: "文字を入力してください", maximum: 10, too_long: "最大%{count}文字まで使用できます" }
 end
