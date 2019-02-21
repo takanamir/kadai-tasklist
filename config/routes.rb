a6910012f0f9c2268c59225f3c28e5557b137276
takanamir <rtakanami0706@gmail.com>
2019-02-21 05:16:53 +0000

    Login and Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: 'toppages#index'
 
   get 'users/index'
-
   get 'users/show'
-
   get 'users/new'
-
   get 'users/create'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   root to: 'tasks#index'
 
