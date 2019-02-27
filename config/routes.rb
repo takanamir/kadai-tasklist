92abb3a55d519210f977d519c35c7df9aee8cd9c
takanamir <rtakanami0706@gmail.com>
2019-02-27 00:21:24 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-
-  get 'users/new'
-  get 'users/create'
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
